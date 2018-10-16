4e5d85fb6f86600d7d11c10feff058d3f7152652
Dylan Shoupe <30540079+Dshoupe@users.noreply.github.com>
2018-10-16 12:04:35 -0600

    Things

--- TestServer/TestServer/Models/Person.cs
+++ TestServer/TestServer/Models/Person.cs
@@ -11,21 +11,19 @@ namespace TestServer.Models
 {
     public class Person
     {
-        public HttpClient Client { get; set; }
         public int Number { get; set; }
         public string Name { get; set; }
 
         public Person(int i)
         {
             Name = $"Person {i}";
-            Client = new HttpClient();
-            Client.BaseAddress = new Uri(Program.url);
         }
 
         public void GetInLine(Object stateInfo)
         {
             Console.WriteLine($"{Name} got in line at {DateTime.Now}. Thread ID: {Thread.CurrentThread.ManagedThreadId}");
-            Client.PostAsync(Program.url, new StringContent(stateInfo.ToString()));
+            Program.GrabNumber(this);
+            Program.WriteName(this);
             Console.WriteLine($"{Name} got in line at {DateTime.Now}. Thread ID: {Thread.CurrentThread.ManagedThreadId}");
         }
     }
--- TestServer/TestServer/Program.cs
+++ TestServer/TestServer/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Sockets;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading;
@@ -17,41 +18,44 @@ namespace TestServer
         public static int waitTimeMax = 1000;
         public static int writeTimeMin = 500;
         public static int writeTimeMax = 1000;
-        public static HttpListener listener;
-        public static string url = "http://localhost:8000/";
-        public static int pageViews = 0;
         public static int TotalCustomer = 1;
         public static int TotalNumOfCustomer = 50;
         public static List<string> CustomerNames = new List<string>();
         public static Semaphore TakeNumberSemaphore;
         public static Random randy = new Random();
         public static int requestCount = 0;
-        public static string pageData =
-            "<!DOCTYPE>" +
-            "<html>" +
-            "  <head>" +
-            "    <title>HttpListener Example</title>" +
-            "  </head>" +
-            "  <body>" +
-            "    <p>Page Views: {0}</p>" +
-            "    <form method=\"post\" action=\"shutdown\">" +
-            "      <input type=\"submit\" value=\"Shutdown\" {1}>" +
-            "    </form>" +
-            "  </body>" +
-            "</html>";
 
         public static void Main(string[] args)
         {
             TakeNumberSemaphore = new Semaphore(TotalNumOfCustomer, TotalNumOfCustomer);
-            listener = new HttpListener();
-            listener.Prefixes.Add(url);
-            listener.Start();
-            Console.WriteLine("Listening for connections on {0}", url);
 
-            Task listenTask = HandleIncomingConnections();
-            listenTask.GetAwaiter().GetResult();
+            IPHostEntry ipHostInfo = Dns.Resolve(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPAddress ipAddressLocal = new IPAddress(new byte[] { 127, 0, 0, 1 });
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddressLocal, 8000);
+            //Console.WriteLine(ipAddress);
+            Console.WriteLine(ipAddressLocal);
 
-            listener.Close();
+            Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            listener.Bind(localEndPoint);
+            listener.Listen(100);
+
+            Console.WriteLine("Waiting for a connection...");
+            Socket handler = listener.Accept();
+            String data = "";
+            byte[] bytes = new byte[1024];
+            while (!data.Contains("exit"))
+            {
+                bytes = new byte[1024];
+                int bytesRec = handler.Receive(bytes);
+                data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+
+                Console.WriteLine("Text received : {0}", data);
+            }
+            byte[] msg = Encoding.ASCII.GetBytes(data);
+            handler.Send(msg);
+            handler.Shutdown(SocketShutdown.Both);
+            handler.Close();
         }
 
         public static void GrabNumber(Person p)
@@ -69,46 +73,6 @@ namespace TestServer
             CustomerNames.Add(p.Name);
             Thread.Sleep(randy.Next(500, 1000));
             Console.WriteLine($"{p.Name} has added their name to the list");
-        }
-
-        public static async Task HandleIncomingConnections()
-        {
-            bool runServer = true;
-
-            while (runServer)
-            {
-                HttpListenerContext ctx = await listener.GetContextAsync();
-
-                HttpListenerRequest req = ctx.Request;
-                HttpListenerResponse resp = ctx.Response;
-
-                Console.WriteLine("Request #: {0}", ++requestCount);
-                Console.WriteLine(req.Url.ToString());
-                Console.WriteLine(req.HttpMethod);
-                Console.WriteLine(req.UserHostName);
-                Console.WriteLine(req.UserAgent);
-                Console.WriteLine();
-
-                if ((req.HttpMethod == "POST") && (req.Url.AbsolutePath == "/shutdown"))
-                {
-                    Console.WriteLine("Shutdown requested");
-                    runServer = false;
-                }
-
-                if (req.Url.AbsolutePath != "/favicon.ico")
-                {
-                    pageViews += 1;
-                }
-
-                string disableSubmit = !runServer ? "disabled" : "";
-                byte[] data = Encoding.UTF8.GetBytes(String.Format(pageData, pageViews, disableSubmit));
-                resp.ContentType = "text/html";
-                resp.ContentEncoding = Encoding.UTF8;
-                resp.ContentLength64 = data.LongLength;
-
-                await resp.OutputStream.WriteAsync(data, 0, data.Length);
-                resp.Close();
-            }
         }
     }
 }
